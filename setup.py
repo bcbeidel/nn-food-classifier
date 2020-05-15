cd84ba632170d9466e2352efc7db7642c2c0f27d
Brandon Beidel <bcbeidel@gmail.com>
2020-05-15 17:21:33 -0400

    init from cookie-cutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Build a multi-class classifier for food items',
+    author='Brandon Beidel',
+    license='MIT',
+)
