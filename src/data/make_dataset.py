ab7086a3080588a1e73f7ae72176ef36b22ec594
Brandon Beidel <bcbeidel@gmail.com>
2020-05-16 10:26:16 -0400

    init functions to read in image data

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -4,6 +4,11 @@ import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
+import os
+import h5py
+
+from src import file_utilities
+
 
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
@@ -14,6 +19,34 @@ def main(input_filepath, output_filepath):
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
+
+
+def get_h5py_dataset(file_name):
+    project_root = file_utilities.get_project_root()
+    data_root    = os.path.join(project_root, "data", "external", "food-101", "h5") 
+    data_path    = os.path.join(data_root, file_name)
+    data         = h5py.File(data_path, "r")
+    return data
+
+
+def get_train_data():
+    data = get_h5py_dataset("food_c101_n10099_r32x32x3.h5")
+
+    X_train = data["images"][:]
+    Y_train = data["category"][:]
+    classes = data["category_names"][:]
+
+    return X_train, Y_train, classes
+
+
+def get_test_data():
+    data = get_h5py_dataset("food_test_c101_n1000_r32x32x3.h5")
+
+    X_test = data["images"][:]
+    Y_test = data["category"][:]
+    classes = data["category_names"][:]
+
+    return X_test, Y_test, classes
 
 
 if __name__ == '__main__':
